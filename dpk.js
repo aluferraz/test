1d95e1b67d4876aabf72cd8da9334287a1301644
Lucas Mohallem Ferraz <ferraz.m.lucas@gmail.com>
2023-02-20 11:25:39 -0300

    Split getPartition and Hash

--- dpk.js
+++ dpk.js
@@ -43,29 +43,27 @@ function getCandidate(event) {
   if (event) {
     if (event.partitionKey) {
       //If the provided partitionKey is too big
-      if (event.partitionKey.length > MAX_PARTITION_KEY_LENGTH) {
-        candidate = hashPartitionKey(event.partitionKey);
-      } else {
-        candidate = event.partitionKey;
-      }
+      candidate = event.partitionKey;
     } else {
-      const data = JSON.stringify(event);
-      candidate = hashPartitionKey(data);
+      candidate = JSON.stringify(event);
     }
   }
+  return getPartitionKey(candidate);
+}
 
-
-  return candidate;
+function getPartitionKey(partitionKey) {
+  if (typeof partitionKey !== "string") {
+    partitionKey = JSON.stringify(partitionKey);
+  }
+  if (partitionKey.length > MAX_PARTITION_KEY_LENGTH) {
+    return hashPartitionKey(partitionKey);
+  }
+  return partitionKey;
 }
 
 function hashPartitionKey(partitionKey) {
-  if (typeof candidate !== "string") {
-    partitionKey = JSON.stringify(partitionKey);
-  }
   return crypto.createHash("sha3-512").update(partitionKey).digest("hex");
 }
-
-
 exports.deterministicPartitionKey = deterministicPartitionKey;
 exports.TRIVIAL_PARTITION_KEY = TRIVIAL_PARTITION_KEY;
 exports.MAX_PARTITION_KEY_LENGTH = MAX_PARTITION_KEY_LENGTH;
